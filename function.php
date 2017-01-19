f5e561a50e261c341dd05f9b5bcdf0d0c513ae76
kRYOoX <me@kry.wtf>
2017-01-19 22:43:24 +0100

    test

--- function.php
+++ function.php
@@ -281,11 +281,8 @@ function ranking()
             if (!$data["map$x"])
                 break ;
 
-            $sql_query = $bdd->prepare(
-                "SELECT match_result.id, match_result.map1, map_result.id, map_result.score_p1, map_result.score_p2 FROM match_result, map_result WHERE match_result.id = :match_id AND match_result.map1 = :map_id"
-            );
-            $sql_query->bindParam(':match_id', $data['id']);
-            $sql_query->bindParam(':map_id', $data["map$x"]);
+            $query = "SELECT match_result.id, match_result." . $data["map$x"] . ", map_result.id, map_result.score_p1, map_result.score_p2 FROM match_result, map_result WHERE match_result.id = " . $data['id'] . " AND match_result." . $data["map$x"] . " = " . $data["map$x"];
+            $sql_query = $bdd->prepare($query);
             $sql_query->execute();
             $map = $sql_query->fetch();
 
