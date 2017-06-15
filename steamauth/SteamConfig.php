f0befec2d50a2b2acc1d1e6f7053a7c00e00701f
Yani <yani.o@hotmail.fr>
2017-06-15 11:51:02 +0200

    Changement page connexion et deconnexion

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -2,8 +2,8 @@
 //Version 3.2
 $steamauth['apikey'] = "8F25D1AE8072C7EEC4D51A90324B549D"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "evilcup.kry.wtf/"; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = "index2.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "index2.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!</div>");}
